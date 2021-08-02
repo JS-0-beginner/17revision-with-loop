b85c33f6982686c09dc0d71b74463a93d9af2a61
avishekataur <avishekataur@gmail.com>
2021-08-02 19:45:57 +0600

    third

---
+++ for-loop.js
@@ -0,0 +1,51 @@
+
+// for (var burgerGiven=0; burgerGiven<9; burgerGiven++)
+// {
+//     console.log('Give burgers with sauces.');
+//     console.log(burgerGiven);
+// }
+
+// var burgerGiven='i';
+
+// for (i=0; i<=9; i++)
+// {
+//     console.log('Give burgers with sauces.');
+//     console.log(i);
+// }
+
+
+/////Array with for loop\\\\\\\\\\\\
+
+// var foods = ['pizza', 'burger', 'fries', 'donut', 'chips', 'cookies']
+
+// console.log('');
+
+// for (var i=0; i<8; i++)
+// {
+//     var foodList = foods[i];
+//     console.log(foodList);
+// }
+// console.log('');
+// console.log('Now use array length');
+// console.log('');
+// var foods = ['pizza', 'burger', 'fries', 'donut', 'chips', 'cookies']
+
+// for (var i=0; i<foods.length; i++)
+// {
+//     var foodList = foods[i];
+//     console.log(foodList);
+// }
+
+// console.log('');
+
+// console.log('Thats how you can print all the elements of an array');
+
+// console.log('');
+
+var numbers = [10, 20, 30, 40, 50, 60, 70];
+
+for (var i=0; i<numbers.length; i++)
+{
+    var list = numbers[i];
+    console.log(list);
+}
