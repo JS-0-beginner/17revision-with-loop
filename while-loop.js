04a7dee8657e66c9c843afaf24e588e269baaac9
avishekataur <avishekataur@gmail.com>
2021-08-02 14:07:34 +0600

    second

--- while-loop.js
+++ while-loop.js
@@ -1,10 +1,38 @@
-var burgerGiven = 0;
+// var burgerGiven = 0;
 
-while (burgerGiven<9)
+// while (burgerGiven<9)
+// {
+//     console.log('Give them burger with sauce.');
+//     burgerGiven++;
+//     console.log(burgerGiven);
+// }
+
+// var number = 1;
+
+// while (number<=10)
+// {
+//     console.log(number);
+//     number++;
+// }
+
+// odd numbers
+
+var number = 1;
+
+// while (number<=20)
+// {
+//     console.log(number);
+//     number+=2;
+// }
+
+number = 0;
+
+while (number<=20)
 {
-    console.log('Give them burger with sauce.');
-    burgerGiven++;
-    console.log(burgerGiven);
+    console.log(number);
+    number+=2;
 }
+
+
 
 
